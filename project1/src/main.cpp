77e3ad804b9d9524b7ba49c348fc92f94d39aa6d
Weirb <billyweir1@gmail.com>
2017-11-13 23:45:49 +0000

    Remove main code and add new header.

--- project1/src/main.cpp
+++ project1/src/main.cpp
@@ -3,22 +3,11 @@
 #include "mmatrix.h"
 #include "mbandedmatrix.h"
 #include "functions.h"
+#include "testing.h"
 
 using namespace std;
 
 int main(int argc, char** argv){
-
-	int n = 5;
-	MBandedMatrix A = laplacian_2d_banded(n);
-	MVector b = create_vector1(n*n);
-	VectorSolution sol = cg(A, b, MVector(n*n, 0), 1000, 1e-6);
-	cout << sol.iteration_count << endl;
-	cout << sol.solution << endl;
-
-	MMatrix B = laplacian_2d(n);
-	sol = cg(B, b, MVector(n*n, 0), 1000, 1e-6);
-	cout << sol.iteration_count << endl;
-	cout << sol.solution;
 
 	return 0;
 }
