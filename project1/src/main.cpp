2c5cc28e40f261866a3a759dcbf2f884eef00a72
Weirb <billyweir1@gmail.com>
2017-11-16 00:26:53 +0000

    Add try/catch block for memory allocation issue.

--- project1/src/main.cpp
+++ project1/src/main.cpp
@@ -8,27 +8,35 @@
 using namespace std;
 
 void generate_all_data() {
+	// This is memory intensive, may cause the machine to hold or crash.
+	// Visual Studio can handle this with 8GB of RAM
 	
-	cout << "create_cg_plot_laplacian_1d" << endl;
-	create_cg_plot_laplacian_1d();
+	try {
+		cout << "create_cg_plot_laplacian_1d" << endl;
+		create_cg_plot_laplacian_1d();
 
-	cout << "create_cg_table_laplacian_1d" << endl;
-	create_cg_table_laplacian_1d();
+		cout << "create_cg_table_laplacian_1d" << endl;
+		create_cg_table_laplacian_1d();
 
-	cout << "create_table_laplacian_1d_banded" << endl; 
-	create_table_laplacian_1d_banded();
+		cout << "create_table_laplacian_1d_banded" << endl; 
+		create_table_laplacian_1d_banded();
 
-	cout << "create_timings_laplace_operator_2d" << endl;
-	create_timings_laplace_operator_2d();
+		cout << "create_timings_laplace_operator_2d" << endl;
+		create_timings_laplace_operator_2d();
 
-	cout << "create_plot_data_laplace_operator_2d" << endl; 
-	create_plot_data_laplace_operator_2d();
+		cout << "create_plot_data_laplace_operator_2d" << endl; 
+		create_plot_data_laplace_operator_2d();
 
-	cout << "create_cg_plot_matrix2" << endl;
-	create_cg_plot_matrix2();
+		cout << "create_cg_plot_matrix2" << endl;
+		create_cg_plot_matrix2();
 
-	cout << "create_cg_table_matrix2" << endl;
-	create_cg_table_matrix2();
+		cout << "create_cg_table_matrix2" << endl;
+		create_cg_table_matrix2();
+
+	} catch(const bad_alloc& e){
+		cout << e.what() 
+			 << "Memory error, could not generate data. Exiting..." << endl;
+	}
 }
 
 int main(int argc, char** argv){
