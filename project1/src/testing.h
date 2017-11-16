4d92109bc09c16d2c84ef91e754332014becdb90
weirb <billyweir1@gmail.com>
2017-11-16 13:24:25 +0000

    New method for comparison of mmatrix and mbandedmatrix.

--- project1/src/testing.h
+++ project1/src/testing.h
@@ -434,6 +434,62 @@ void create_table_laplacian_1d_banded(){
     f.close();
 }
 
+// Comparison_tabulated
+void create_timings_laplace_operator_1d(){
+// Find execution times for computing the solution using the CG algorithm
+// for MMatrix and MBandedMatrix
+
+    // Output file
+    ofstream f("1d_timings.dat");
+    if (!f.is_open()){
+        cerr << "Unable to open \"1d_timings.dat\" for writing.\n"
+             << "Exiting...\n";
+        exit(-1);
+    }
+
+    // Variables for computing cg
+    MMatrix A1;
+    MBandedMatrix A2;
+    MVector b, x0;
+    int maxiter = 1000;
+    double tol = 1e-6;
+
+    // Timing variables
+    double start1 = 0, end1 = 0;
+    double start2 = 0, end2 = 0;
+
+    for (int k = 0; k < 15; ++k ){
+        int n = 5 * pow(2,k);
+        A1 = laplacian_1d(n);
+        A2 = laplacian_1d_banded(n);
+        b = create_vector1(n);
+        x0 = MVector(n, 0.);
+
+        // Time CG using MMatrix
+        start1 = Timer();
+        VectorSolution sol1 = cg(A1, b, x0, maxiter, tol);
+        end1 = Timer();
+
+        // Time CG using MBandedMatrix
+        start2 = Timer();
+        VectorSolution sol2 = cg(A2, b, x0, maxiter, tol);
+        end2 = Timer();
+
+        // Output format: tab separated values
+        //  size of matrix;
+        //  time to execute cg for MMatrix;
+        //  iterations to converge for MMatrix;
+        //  time to execute cg for MBandedMatrix;
+        //  iterations to converge for MBandedMatrix;
+        f << n << "\t" << (end1 - start1) << "\t" 
+                       << sol1.iteration_count << "\t"
+                       << (end2 - start2) << "\t" 
+                       << sol2.iteration_count << endl;
+    }
+
+    f.close();
+}
+
 // Task 3.3.4
 void create_timings_laplace_operator_2d(){
 // Find execution times for computing the solution using the CG algorithm
