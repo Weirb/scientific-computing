bf124220296902fe202ae5745e6413176304c05e
Weirb <billyweir1@gmail.com>
2017-12-09 13:53:01 +0000

    Square wave initial condition.

--- project2/gen_plots.m
+++ project2/gen_plots.m
@@ -1,5 +1,6 @@
 function [] = gen_plots()
 
+animate_plot();
 end
 
 function [] = plot1()
@@ -41,18 +42,34 @@ function [] = plot_3d()
     zlabel('u(x,t)');
 end
 
+function [z] = square_wave(x)
+if (0<= x) && (x <= 1)
+    z = 1.5;
+else
+    z = 0;
+end
+end
+
 function [] = animate_plot()
-    N = 32;
-    data = load('plot_data.dat');
+    N = 10;
+    data = load('animation_data.dat');
     y = data(1:N, :);
     x = linspace(0,2*pi,50);
-
+    
+    figure();
+    hold on;
     h = plot(NaN,NaN);
+    k = plot(NaN,NaN);
     axis([min(x) max(x) min(min(y)) max(max(y))]);
-
-    for k = 1:N
+    legend('Approximate', 'Exact');
+    for i = 1:N
         pause(0.1)
-        set(h, 'XData', x, 'YData', y(k,:));
+%         ex = 1.5+sin(x-0.05*(i-1));
+%         ex = square_wave(x-0.05*(i-1));
+%         ex = (0 <= x) .* (x <= 1)
+        ex = square(x);
+%         set(h, 'XData', x, 'YData', y(i,:));
+        set(k, 'XData', x, 'YData', ex);
         drawnow
     end
 end
