3cdb1b5aa7c8dce74b3377df969b8c21fbb11858
weirb <billyweir1@gmail.com>
2017-12-10 19:57:56 +0000

    Add comments detailing method.

--- project2/src/mesh.h
+++ project2/src/mesh.h
@@ -31,24 +31,30 @@ template<class Element>
 class Mesh {
 
 public:
+	// Number of elements in the mesh
 	int n_elements;
 	
     // Empty constructor
-    Mesh() : n_elements(0), elements(nullptr){}
+    Mesh() : Mesh(0, 0, 0, nullptr){}
 
-    // Constructor
+    // Constructor initialises the mesh for the given arguments
     Mesh(int mesh_size, double x0, double x1, double (*initial_condition)(double x)){
 		n_elements = mesh_size;
         if (mesh_size > 0)
 		    create_mesh(mesh_size, x0, x1, initial_condition);
 	}
     
+	/*
+	Calculate interpolated values of the data on the elements.
+	Parameter s is a real number between -1 and 1.
+	*/
     MeshData get_mesh_data(double s){
 
         MeshData data(n_elements);
         
-        AdvectionElement* e = elements;
-        for (int i = 0; i < n_elements; ++i, e=e->Right_neighbour_pt){
+		// Loop over all the elements, calculating the interplated values
+		// on each element.
+        for (int i = 0; i < n_elements; ++i, elements=elements->Right_neighbour_pt){
             data.x[i] = e->interpolated_x(s);
             data.u[i] = e->interpolated_u(s);
         }
@@ -56,15 +62,26 @@ public:
         return data;
     }
 
+	/*
+	Timestep the solution on the mesh.
+	To perform a single timestep, there are two loops:
+
+	(1) For each element e
+			Calculate the solution on e
+	(2) For each element e
+			Update the solution on e
+	*/
 	void timestep(double dt) {
 
-		// Create the inverse of the mass matrix
+		// Create the inverse of the mass matrix.
+		// The matrix is the same for each element since we have
+		// a uniform mesh.
 		MMatrix Minv(2, 2, 0.);
 		Minv(0, 0) = 2; Minv(0, 1) = -1;
 		Minv(1, 0) = -1; Minv(1, 1) = 2;
 		Minv = Minv * (2. / (elements->X[1] - elements->X[0]));
 
-		// Compute the update values
+		// (1) Loop over each element and compute the update values 
 		for (int i = 0; i < n_elements; ++i, elements=elements->Right_neighbour_pt){
 			
 			// Create temporary storage for the updated values
@@ -82,11 +99,11 @@ public:
 			H(0) = elements->h(elements->Left_neighbour_pt->U[1], elements->U[0]);
 			H(1) = -elements->h(elements->U[1], elements->Right_neighbour_pt->U[0]);
 			
-			// Perform the update step
+			// Perform the update step for the temporary storage
 			elements->U_update = elements->U_update + dt*Minv*(F+H);
 		}
 
-		// Update the values into U
+		// (2) Loop over each element and update the values into permanent U
 		for (int i = 0; i < n_elements; ++i, elements=elements->Right_neighbour_pt){
 			elements->U[0] = elements->U_update[0];
 			elements->U[1] = elements->U_update[1];
