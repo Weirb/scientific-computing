54e76b20fc58527ddeadb7206db457aa260d27fb
Weirb <billyweir1@gmail.com>
2017-12-12 22:41:58 +0000

    Change up the plotting functions.

--- project2/gen_plots.m
+++ project2/gen_plots.m
@@ -1,26 +1,103 @@
 function [] = gen_plots()
 
-animate_plot_file('data2.dat');
+
+hold on;
+data = load('data/burgers_sin_t_1.dat');
+plot(data(:,1),data(:,2), 'ko', 'markers',8);
+
+x = linspace(0,2*pi,1000); 
+
+t=0.5; 
+u=(2*sinh(x))./(cosh(x)-exp(-t)); 
+plot(x,u,'--'); 
+% for t=0.2:0.5:1.2 
+%     u=(-2*sinh(x))./(cosh(x)-exp(-t)); 
+%     plot(x,u); 
+% end
+
 end
 
-function [] = plot1()
-    files = ["plot_data_10.dat";"plot_data_100.dat";"plot_data_200.dat"];
-    hold on;
-    for i = 1:3
-        data = load(files(i,:));
-        plot(data(:,1),data(:,2));
-    end
-    legend('10','100','200');
+function [x] = square(x)
+x = (0 <= x).*(x <= 1);
 end
 
-function [] = plot2(c, dt)
-    data = load('plot_data.dat');
-    x = data(:,1);
+function [] = plot_burgers_sine(filename, t)
+    figure();
     hold on;
-    plot(x, data(:,2), 'rx');
-    plot(x, 1.5+sin(x-c*dt));
-    legend('Approximation', 'Exact');
+    grid on;
+    
+%     x = linspace(0,2*pi,1000);
+%     plot(x,1.5+sin(x-t),'-', 'LineWidth',2,'Color',[1,0.4,0]);
+    
+    data = load(filename);
+    plot(data(:,1),data(:,2), 'ko', 'markers',8);
+    
+    xlabel('x');
+    ylabel('u(x,t)');
+    legend('Exact solution', 'Approximate solution');
+    axis([ 0 2*pi 0 3]);
+    set(findall(gcf,'-property','FontSize'),'FontSize',20);
+    set(gcf, 'Units', 'Inches', 'Position', [0, 0, 9, 6]);
+    [~,name,~] = fileparts(filename);
+%     saveas(gcf, sprintf('figures/%s', name), 'epsc');
 end
+
+function [] = plot_advection_square(filename, t)
+    figure();
+    hold on;
+    grid on;
+    
+    x = linspace(0,2*pi,1000);
+    plot(x,square(x-t),'-', 'LineWidth',2,'Color',[1,0.4,0]);
+    
+    data = load(filename);
+    plot(data(:,1),data(:,2), 'ko', 'markers',8);
+    
+    xlabel('x');
+    ylabel('u(x,t)');
+    legend('Exact solution', 'Approximate solution');
+    axis([ 0 2*pi -0.2 1.4]);
+    set(findall(gcf,'-property','FontSize'),'FontSize',20);
+    set(gcf, 'Units', 'Inches', 'Position', [0, 0, 9, 6]);
+    [~,name,~] = fileparts(filename);
+    saveas(gcf, sprintf('figures/%s', name), 'epsc');
+end
+
+function [] = plot_advection_sine(filename, t)
+    figure();
+    hold on;
+    grid on;
+    
+    x = linspace(0,2*pi,1000);
+    plot(x,1.5+sin(x-t),'-', 'LineWidth',2,'Color',[1,0.4,0]);
+    
+    data = load(filename);
+    plot(data(:,1),data(:,2), 'ko', 'markers',8);
+    
+    xlabel('x');
+    ylabel('u(x,t)');
+    legend('Exact solution', 'Approximate solution');
+    axis([ 0 2*pi 0 3]);
+    set(findall(gcf,'-property','FontSize'),'FontSize',20);
+    set(gcf, 'Units', 'Inches', 'Position', [0, 0, 9, 6]);
+    [~,name,~] = fileparts(filename);
+    saveas(gcf, sprintf('figures/%s', name), 'epsc');
+end
+
+function [] = plot_advection_all()
+% SINE WAVES
+plot_advection_sine('data/advec_sin_t_0.dat', 0);
+plot_advection_sine('data/advec_sin_t_1.dat', 0.25);
+plot_advection_sine('data/advec_sin_t_2.dat', 0.5);
+plot_advection_sine('data/advec_sin_t_3.dat', 1);
+
+% SQUARE WAVES
+plot_advection_square('data/advec_sq_t_0.dat', 0);
+plot_advection_square('data/advec_sq_t_1.dat', 0.25);
+plot_advection_square('data/advec_sq_t_2.dat', 0.5);
+plot_advection_square('data/advec_sq_t_3.dat', 1);
+end
+
 
 function [] = plot_3d()
     N = 28;
@@ -42,14 +119,6 @@ function [] = plot_3d()
     zlabel('u(x,t)');
 end
 
-function [z] = square_wave(x)
-if (0<= x) && (x <= 1)
-    z = 1.5;
-else
-    z = 0;
-end
-end
-
 function [] = animate_plot_file(filename)
     data = load(filename);
     [N, M] = size(data);
@@ -64,29 +133,5 @@ function [] = animate_plot_file(filename)
         set(h, 'XData', x);
         set(h, 'YData', y(i,:));
         drawnow;
-    end
-end
-
-function [] = animate_plot()
-    N = 10;
-    data = load('animation_data.dat');
-    y = data(1:N, :);
-    x = linspace(0,2*pi,50);
-    
-    figure();
-    hold on;
-    h = plot(NaN,NaN);
-    k = plot(NaN,NaN);
-    axis([min(x) max(x) min(min(y)) max(max(y))]);
-    legend('Approximate', 'Exact');
-    for i = 1:N
-        pause(0.1)
-%         ex = 1.5+sin(x-0.05*(i-1));
-%         ex = square_wave(x-0.05*(i-1));
-%         ex = (0 <= x) .* (x <= 1)
-        ex = square(x);
-%         set(h, 'XData', x, 'YData', y(i,:));
-        set(k, 'XData', x, 'YData', ex);
-        drawnow
     end
 end
