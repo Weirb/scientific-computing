faa0111882ae6b4759e51aff14d22e82579081e1
Weirb <billyweir1@gmail.com>
2017-12-14 21:36:56 +0000

    Complete generate data code.

--- project2/src/main.cpp
+++ project2/src/main.cpp
@@ -59,31 +59,32 @@ void generate_data(){
 	auto sine_ic = [](double x) { return 1.5 + sin(x); };
 	
 	vector<double> t1 = {0., 0.25, 0.5, 1.};
-	vector<double> t2 = {0., 0.5, 1., 2.};
+	vector<double> t2 = {0., 0.25, 0.5, 1., 1.25, 1.5, 1.75, 2.};
 	vector<string> files_a = {"data/advec_sin_t_0.dat", "data/advec_sin_t_1.dat", "data/advec_sin_t_2.dat", "data/advec_sin_t_3.dat",
 							  "data/advec_sq_t_0.dat", "data/advec_sq_t_1.dat", "data/advec_sq_t_2.dat", "data/advec_sq_t_3.dat"};
 	vector<string> files_b = {"data/burgers_sin_t_0.dat", "data/burgers_sin_t_1.dat", "data/burgers_sin_t_2.dat", "data/burgers_sin_t_3.dat",
-							  "data/burgers_sq_t_0.dat", "data/burgers_sq_t_1.dat", "data/burgers_sq_t_2.dat", "data/burgers_sq_t_3.dat"};
+						      "data/burgers_sin_t_4.dat", "data/burgers_sin_t_5.dat", "data/burgers_sin_t_6.dat", "data/burgers_sin_t_7.dat",
+							  "data/burgers_sq_t_0.dat", "data/burgers_sq_t_1.dat", "data/burgers_sq_t_2.dat", "data/burgers_sq_t_3.dat",
+							  "data/burgers_sq_t_4.dat", "data/burgers_sq_t_5.dat", "data/burgers_sq_t_6.dat", "data/burgers_sq_t_7.dat"};
 
 	int N = 100;
 	double dx = 2 * M_PI / (double)N;
 	double dt = 1e-4;
 	cout << (dt / dx) << endl;
 
-	// Mesh<AdvectionElement> mesh_a;
+	Mesh<AdvectionElement> mesh_a;
 
-	// for (int i = 0; i < t1.size(); ++ i) {
-	// 	int iteration_count = t1[i] / dt;
-	// 	mesh_a = Mesh<AdvectionElement>(N, 0, 2 * M_PI, sine_ic);
-	// 	create_data_file(mesh_a, dt, iteration_count, files_a[i]);
-	// }
+	for (int i = 0; i < t1.size(); ++ i) {
+		int iteration_count = t1[i] / dt;
+		mesh_a = Mesh<AdvectionElement>(N, 0, 2 * M_PI, sine_ic);
+		create_data_file(mesh_a, dt, iteration_count, files_a[i]);
+	}
 	
-	// for (int i = 0; i < t1.size(); ++ i) {
-	// 	int iteration_count = t1[i] / dt;
-	// 	mesh_a = Mesh<AdvectionElement>(N, 0, 2 * M_PI, square_ic);
-	// 	create_data_file(mesh_a, dt, iteration_count, files_a[t1.size()+i]);
-	// }
-
+	for (int i = 0; i < t1.size(); ++ i) {
+		int iteration_count = t1[i] / dt;
+		mesh_a = Mesh<AdvectionElement>(N, 0, 2 * M_PI, square_ic);
+		create_data_file(mesh_a, dt, iteration_count, files_a[t1.size()+i]);
+	}
 
 	Mesh<BurgersElement> mesh_b;
 
@@ -95,36 +96,13 @@ void generate_data(){
 
 	for (int i = 0; i < t2.size(); ++ i) {
 		int iteration_count = t2[i] / dt;
-		mesh_b = Mesh<BurgersElement>(N, 0, 2 * M_PI, sine_ic);
+		mesh_b = Mesh<BurgersElement>(N, 0, 2 * M_PI, square_ic);
 		create_data_file(mesh_b, dt, iteration_count, files_b[t2.size()+i]);
 	}
 }
 
 int main(int argc, char** argv){
-	// auto square_ic = [](double x) { return (0 <= x) && (x <= 1) ? 1. : 0.; };
-	// auto sine_ic = [](double x) { return 1.5 + sin(x); };
-	
-	// int N = 200;
-	// double dx = 2 * M_PI / (double)N;
-	// // double dt = 1e-4;
-	// double dt = dx / 64;
-
-	// double T = 4;
-	// int iteration_count = T / dt;
-
-	// Mesh<AdvectionElement> mesh1(N, 0, 2*M_PI, square_ic);
-	// Mesh<BurgerElement> mesh2(N, 0, 2*M_PI, sine_ic);
-
-	// Mesh<AdvectionElement> mesh2(10, 0, 2*M_PI, sine_ic);
-	// MeshData data = mesh2.get_mesh_data(0);
-	// for (int i = 0; i < 10; ++i){
-	// 	cout << data.x[i] << ", " << data.u[i] << endl;
-	// }
-
-	// create_animation_data(mesh1, dt, iteration_count, "data1.dat");
-	// create_animation_data(mesh2, dt, iteration_count, "data2.dat");
 
 	generate_data();
-
 	return 0;
 }
